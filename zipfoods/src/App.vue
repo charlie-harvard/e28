99f709348ba492cde8d2c95c512320a8f32c5d45
Charlie <chg774@g.harvard.edu>
2019-11-06 10:01:59 -0500

    [zipfoods] Add ShowHome

--- zipfoods/src/App.vue
+++ zipfoods/src/App.vue
@@ -1,27 +1,22 @@
 <template>
   <div id="app">
     <img id="logo" alt="ZipFoods Logo" src="./assets/images/zipfoods-logo.png">
-    <p>ZipFoods is your one-stop-shop for convenient online grocery shopping in the greater Boston area.</p>
     
     <show-catagories></show-catagories>
-    
-    <show-featured :category='"snacks"'></show-featured>
-    
+    <show-home></show-home>
     <show-products></show-products>
   </div>
 </template>
 
 <script>
 import ShowCatagories from './components/ShowCatagories.vue'
-import ShowFeatured from './components/ShowFeatured.vue'
+import ShowHome from './components/ShowHome.vue'
 import ShowProducts from './components/ShowProducts.vue'
-
-
 
 export default {
   name: 'app',
   components: {
-    ShowCatagories, ShowFeatured, ShowProducts
+    ShowCatagories, ShowHome, ShowProducts
   }
 }
 </script>
