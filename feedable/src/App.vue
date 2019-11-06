3b1390aa366e9fe2712c7e50328e6a9c17e070d5
Charlie <chg774@g.harvard.edu>
2019-11-06 13:55:16 -0500

    [Project 3, 4] Initialize Feedable App

---
+++ feedable/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <HelloWorld msg="Welcome to Feedable App"/>
+    <img alt="Vue logo" src="./assets/logo.png" style="width:256px;height:256px">
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+</style>
