6a112f5fe35ff961f98c0ca559658cd2d4880b23
Charlie Gao <chg774@g.harvard.edu>
2019-12-17 16:24:59 -0500

    Remove notes function

--- p4/src/components/pages/MyChannels.vue
+++ p4/src/components/pages/MyChannels.vue
@@ -13,7 +13,6 @@
           >{{ channel.domain }}</router-link>
           <br />
         </p>
-        <p>Notes: <input type="text" v-model="channel.notes" @change="updateChannel()"></p>
         <p>
           <button class="removeButton" @click="removeChannel(channel)">Remove this channel</button>
         </p>
@@ -38,14 +37,6 @@ export default {
       app.axios.get(app.config.myChannels).then(response => {
         this.myChannels = response.data.myChannels;
       });
-    },
-    updateChannel: function() {
-      let newMyChannels = [];
-      for (let i = 0; i < this.myChannels.length; i++) {
-        newMyChannels.push(this.myChannels[i]);
-      }
-      this.myChannels = this.uniqueChannel(newMyChannels);
-      this.updateMyChannels(newMyChannels);
     },
     removeChannel: function(channel) {
       let newMyChannels = [];
