2b9e0621a493090ab7f4328c2ac033bce83c9845
Charlie Gao <chg774@g.harvard.edu>
2019-12-17 17:05:05 -0500

    Updates

--- p4/tests/e2e/specs/homePage.js
+++ p4/tests/e2e/specs/homePage.js
@@ -1,12 +1,12 @@
 // https://docs.cypress.io/api/introduction/api.html
 
 describe('Feedable Landing Page', () => {
-  it('visits the root url', () => {
+  it('visits the root url and add channl to My Channels', () => {
     cy.visit('/');
     cy.contains('h2', 'Latest News');
     cy.get('.newsItem').its('length').should('be.gte', 1);
     cy.get('.txtlink').first().click();
-    cy.contains('a', 'Read more ...');
     cy.get('#addChannleBtn').click();
+    cy.contains('p', 'You have 1 newly saved channel(s).').should('exist');
   })
 })
